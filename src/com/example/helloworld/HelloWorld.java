340a5501fdfe95183bb89ffd97dd51c68aa38756
Usman Jamil <usman.jamil@live.co.uk>
2016-02-13 22:56:51 +0000

    printing 'Hello World'

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by UsmaanJ on 13/02/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+    System.out.println("Hello World!");
+    }
+}
